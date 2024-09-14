a02555d724a3797e51dfa88a4f4b1acc414bdc65
WeAreInSpace <weareinspace.net@gmail.com>
2024-09-14 12:11:44 +0700

    rebuild

--- Prism-API/src/main/java/org/bukkit/Art.java
+++ Prism-API/src/main/java/org/bukkit/Art.java
@@ -10,11 +10,32 @@ import com.google.common.collect.Maps;
  * Represents the art on a painting
  */
 public enum Art {
-	KEBAB(0, 1, 1), AZTEC(1, 1, 1), ALBAN(2, 1, 1), AZTEC2(3, 1, 1), BOMB(4, 1, 1), PLANT(5, 1, 1), WASTELAND(6, 1, 1),
-	POOL(7, 2, 1), COURBET(8, 2, 1), SEA(9, 2, 1), SUNSET(10, 2, 1), CREEBET(11, 2, 1), WANDERER(12, 1, 2),
-	GRAHAM(13, 1, 2), MATCH(14, 2, 2), BUST(15, 2, 2), STAGE(16, 2, 2), VOID(17, 2, 2), SKULL_AND_ROSES(18, 2, 2),
-	WITHER(19, 2, 2), FIGHTERS(20, 4, 2), POINTER(21, 4, 4), PIGSCENE(22, 4, 4), BURNINGSKULL(23, 4, 4),
-	SKELETON(24, 4, 3), DONKEYKONG(25, 4, 3);
+	KEBAB(0, 1, 1),
+	AZTEC(1, 1, 1),
+	ALBAN(2, 1, 1),
+	AZTEC2(3, 1, 1),
+	BOMB(4, 1, 1),
+	PLANT(5, 1, 1),
+	WASTELAND(6, 1, 1),
+	POOL(7, 2, 1),
+	COURBET(8, 2, 1),
+	SEA(9, 2, 1),
+	SUNSET(10, 2, 1),
+	CREEBET(11, 2, 1),
+	WANDERER(12, 1, 2),
+	GRAHAM(13, 1, 2),
+	MATCH(14, 2, 2),
+	BUST(15, 2, 2),
+	STAGE(16, 2, 2),
+	VOID(17, 2, 2),
+	SKULL_AND_ROSES(18, 2, 2),
+	WITHER(19, 2, 2),
+	FIGHTERS(20, 4, 2),
+	POINTER(21, 4, 4),
+	PIGSCENE(22, 4, 4),
+	BURNINGSKULL(23, 4, 4),
+	SKELETON(24, 4, 3),
+	DONKEYKONG(25, 4, 3);
 
 	private int id, width, height;
 	private static final HashMap<String, Art> BY_NAME = Maps.newHashMap();
